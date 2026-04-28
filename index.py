e680133ff455a4a97f14cc66ea2e399007df7168
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-04-28 18:09:08 +0430

    function is done

--- index.py
+++ index.py
@@ -1,4 +1,61 @@
 
-name = "hello world"
 
-print(name)
+
+# def greet():
+    
+#     x = 10
+#     # print(x)
+
+# greet()
+
+# def x():
+
+
+# def y 
+
+# def greet(name):
+#     print("your name is " , name)
+
+# greet("developer")
+
+# def add(a,b):
+#     return a + b
+
+# num = add(3,3)
+
+# print(num)
+
+# def greet(name="developer"):
+#     print("your name is" , name)
+
+
+# greet()
+
+# def student(name , age):
+#     print(f"your name is {name} and your age is {age}")
+
+
+# student(name="alawoddin" , age=20)
+# name = student("alawoddin" , 20)
+
+# student("develolper" , 20)
+# student(name="developer" ,)
+
+
+# print(name)
+
+# add()
+# num = add(2,5)
+
+# print(num)
+
+# def number(*num):
+#     print(f"your number is {num}")
+
+# number(1,2,3,4,5)
+
+
+def info(**data):
+    print(data)
+
+info(name="alawoddin" , age=20 , job="developer")
